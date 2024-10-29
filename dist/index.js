d8e305ef4a6c5dd9629f8e14632aa1e81366756f
MariaShakeel10 <mariashakeel10a@gmail.com>
2024-10-29 18:04:18 +0500

    type annotations & setup

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let myVariable = "hi";
+const myNumber = 78; // Use a different name here
+console.log(myVariable);
+console.log(myNumber);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let myVariable: any = "hi";
+const myNumber: number = 78; // Use a different name here
+console.log(myVariable);
+console.log(myNumber);
