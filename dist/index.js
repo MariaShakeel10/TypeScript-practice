7e99a93d518996c3d1ee03e71c1f975d6e5cf2f0
MariaShakeel10 <mariashakeel10a@gmail.com>
2024-10-29 18:41:00 +0500

    fundamentals tuples

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,33 @@
 "use strict";
+// type annotations
 let myVariable = "hi";
-const myNumber = 78; // Use a different name here
+let age = 78;
 console.log(myVariable);
-console.log(myNumber);
+console.log(age);
+// map & debugging
+if (age < 100)
+    age += 10;
+// Fundamentals // typescript is smart in considering the fundamentals
+let sales = 123456;
+let course = "Typescript";
+let is_available = true;
+// avoid using any
+let level;
+level = 1;
+level = "a";
+/*function render(document: any) {
+   console.log(document);
+   
+}*/
+// arrays
+// let number:number[]=[1,2,3,'4']//error with tring in 4
+let number = [];
+number.forEach(n => n);
+// Tuples
+// it is a fixed length array where each element has a particular.(useful for only two values)
+// restricting your tuples to 2 values is easy to understand the code.
+let user = [1, "abc"];
+// user[0].
+// user[1].
+// let user:[number,string]=[1,"abc","a",1] //error
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
+// type annotations
 let myVariable: any = "hi";
-const myNumber: number = 78; // Use a different name here
+let age: number = 78; 
 console.log(myVariable);
-console.log(myNumber);
+console.log(age);
+// map & debugging
+if (age<100) 
+    age +=10
+// Fundamentals // typescript is smart in considering the fundamentals
+let sales:number=123_456
+let course:string="Typescript"
+let is_available:boolean=true
+// avoid using any
+let level;
+level=1
+level="a"
+
+ /*function render(document: any) {
+    console.log(document);
+    
+ }*/
+// arrays
+// let number:number[]=[1,2,3,'4']//error with tring in 4
+let number:number[]=[]
+number.forEach(n=>n)
+// Tuples
+// it is a fixed length array where each element has a particular.(useful for only two values)
+// restricting your tuples to 2 values is easy to understand the code.
+let user:[number,string]=[1,"abc"]
+// user[0].
+// user[1].
+// let user:[number,string]=[1,"abc","a",1] //error
